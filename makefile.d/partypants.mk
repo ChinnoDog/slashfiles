c18e092d87021aea3861d5feeaf8d72e8820382a
Stephen Nichols <ChinnoDog@lonesheep.net>
2025-06-29 21:56:48 -0400

    Final edits before surrender

--- makefile.d/partypants.mk
+++ makefile.d/partypants.mk
@@ -2,18 +2,39 @@
 
 ifeq ($(HOSTNAME),partypants)
 
-$(HOSTNAME): FORCE /etc/slashfiles/local/grub.cfg
+GRUBCFG := /boot/efi/EFI/debian/grub.cfg
 
-/etc/slashfiles/local/grub.cfg: $(THIS_MAKEFILE)
+$(HOSTNAME): FORCE $(GRUBCFG)
+
+$(GRUBCFG): $(THIS_MAKEFILE)
 	UUIDS="$$(for i in 0 1; do blkid -s UUID -o value /dev/nvme$${i}n1p2 | tr -d "-"; done)"
 	UUID1=$$(grub-probe --target=cryptodisk_uuid /boot)
 	UUID2=$$(echo "$$UUIDS" | grep -v "$$UUID1")
+	ROOT_UUID=$$(blkid -s UUID -o value /dev/mapper/crypt0 | tr -d '-')
+	INSMODS="$$(bash -i -c 'source /usr/lib/grub/grub-mkconfig_lib; prepare_grub_to_access_device /dev/mapper/crypt0 | grep insmod')"
 	mkdir -p $$(dirname $@)
 	cat > "$@" <<EOF
-	cryptomount -u $$UUID1
-	cryptomount -u $$UUID2
-	set root=(crypto0)
-	set prefix=(\$$root)/@/grub
+	insmod all_video
+	insmod efi_gop
+	insmod efi_uga
+	insmod vbe
+	insmod disk
+	insmod scsi
+	insmod ahci
+	insmod usb
+	insmod usbms
+	insmod search
+	insmod search_fs_uuid
+	insmod ext2
+	insmod loopback
+	$$INSMODS
+	search --file --set=imgdev /crypto-keyfile.img
+	loopback loop "($imgdev)/crypto-keyfile.img"
+	cryptomount -l loop || echo "Failed to unlock key container"
+	cryptomount -u $$UUID1 -k "(crypto0)/crypto-keyfile.bin" || echo "Failed to unlock crypt0"
+	cryptomount -u $$UUID2 -k "(crypto0)/crypto-keyfile.bin" || echo "Failed to unlock crypt1"
+	set root='cryptouuid/$$ROOT_UUID'
+	set prefix=(\$$root)/@/boot/grub
 	configfile \$$prefix/grub.cfg
 	EOF
 
